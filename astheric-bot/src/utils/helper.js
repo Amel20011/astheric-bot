59db0b471e91f9cea82580372bf7ac0e5e926e63
Amel20011 <amelcns8@gmail.com>
2025-12-14 17:59:01 +0800

    Create helper.js

---
+++ astheric-bot/src/utils/helper.js
@@ -0,0 +1,7 @@
+// Fungsi-fungsi bantuan (helper functions) bisa ditaruh di sini
+// Contoh:
+// function formatNumber(num) {
+//   return num.toString().padStart(2, '0');
+// }
+
+// module.exports = { formatNumber };
